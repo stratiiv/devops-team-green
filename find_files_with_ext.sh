83a9b41444213ce6c6da93b9a06ed266ebba7778
lisa <el.voievutska@gmail.com>
2023-10-03 13:50:22 +0300

    [UPDATE]  - add comments

--- find_files_with_ext.sh
+++ find_files_with_ext.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+# Check if the number of command-line arguments is proper
 if [ $# -ne 2 ]; then
-    echo "Usage: $0 <path> <extention>"
+    echo "Usage: $0 <path> <extension>"
     exit 1
 fi
 
 dir="$1"
 ext="$2"
 
+# Check if the provided directory path does not exist
 if [ ! -d "$dir" ]; then
-    echo "No such a directory"
+    echo "No such directory"
     exit 1
 fi
 
+# Change the current working directory to the provided directory path or exit if unsuccessful
 cd "$dir" || exit 1
+
+# Use the 'find' command to search for files with the specified extension in the current directory and its subdirectories
 find . -type f -name "*$ext" -print
